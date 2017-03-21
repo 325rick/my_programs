6e7e25779162a7047f2bcb6f635aa2a351e9e1f1
Rick Nygaard <nygaard.richard@mcm.edu>
2017-03-21 00:26:50 -0500

    Add files via upload

--- 4310/playground.c
+++ 4310/playground.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
+#define MAXSIZE 127
+
+void prompt();
+
+int main(int argc, char *argv){
+    argc =2;
+/* Check for expected command line arg */
+    if (argc != 2) {
+        printf("\nMissing Argument\n");
+        printf("\nPress ENTER to continue...\n");
+        getchar();
+        exit(1);
+    }
+// variables
+int i = 0;
+char *ch;
+
+printf("Type -1 to Exit::");
+printf("Enter command::");
+
+do{
+  prompt();
+  fgets(ch, MAXSIZE, stdin);
+  printf("%s", ch);
+  i = atoi(ch);
+
+} while(i != -1);
+
+return 0;
+}
+void prompt(){
+printf("::");
+}
